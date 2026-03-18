6076db432e70b733b554778be174c1e449572849
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-03-18 10:13:38 +0800

    feat: add nix flake support

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Blog Environment";
+
+  inputs = {
+    flake-compat = {
+      url = "https://git.lix.systems/lix-project/flake-compat/archive/main.tar.gz";
+    };
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts/main";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+  };
+
+  outputs =
+    { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShellNoCC {
+            packages = with pkgs; [
+              hugo
+              just
+              nixfmt
+              nixfmt-tree
+            ];
+          };
+        };
+    };
+}
